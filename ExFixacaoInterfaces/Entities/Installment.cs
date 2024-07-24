8383cd4245dca322caddc2df7200fa0dda8ad59a
franchesco aprigio <franchescodsa@outlook.com>
2024-07-24 12:08:07 -0300

    Adicionar arquivos de projeto.

---
+++ ExFixacaoInterfaces/Entities/Installment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExFixacaoInterfaces.Entities
+{
+    internal class Installment
+    {
+
+       
+        public Installment()
+        {
+        }
+
+        public  DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return DueDate.ToString("dd/MM/yyyy")
+                + " - "
+                + Amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
---
+++ ExFixacaoInterfaces/Services/PaypalService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExFixacaoInterfaces.Services
+{
+    internal class PaypalService : IOnlinePaymentService
+    {
+        private const double FeePercentage = 0.02;
+        private const double MothlyInterest = 0.01;
+
+        public double Interest (double amount, int months)
+        {
+            return amount * MothlyInterest * months;
+        }
+        public double PaymentFee(double amount)
+        {
+            return amount * FeePercentage;
+        }
+    }
+}
